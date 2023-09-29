ccb4b62f05f3ed6970253361632503d9f9b88aea
SAFIRA PATEL <121423308+safirapatel@users.noreply.github.com>
2023-09-29 23:19:18 +0530

    Add files via upload

---
+++ productdetail.js
@@ -0,0 +1,88 @@
+const Productdetail =
+[
+    {
+        id:1,
+        //Title:"Milk",
+        Cat: 'Milk',
+        Price: '70/l',
+        Img: './img/milk.jpg'
+    },
+    {
+        id:2,
+        //Title:"Butter",
+        Cat: 'Butter',
+        Price: '200/kg',
+        Img: './img/butter.jpg'
+    },
+    {
+        id:3,
+        //Title:"Butter Milk",
+        Cat: 'Butter Milk',
+        Price: '80/l',
+        Img: './img/buttermilk.jpg'
+    },
+    {
+        id:4,
+        //Title:"Paneer",
+        Cat: 'Paneer',
+        Price: '380/kg',
+        Img: './img/paneer.jpg'
+    },
+    {
+        id:5,
+        //Title:"Yogurt",
+        Cat: 'Yogurt',
+        Price: '270/kg',
+        Img: './img/yogurt.jpg'
+    },
+    {
+        id:6,
+        //Title:"Ghee",
+        Cat: 'Ghee',
+        Price: '1300/kg',
+        Img: './img/ghee.jpg'
+    },
+    {
+        id:7,
+        //Title:"Curd",
+        Cat: 'Curd',
+        Price: '100/kg',
+        Img: './img/curd.jpg'
+    },
+    {
+        id:8,
+        //Title:"Cheese",
+        Cat: 'Cheese',
+        Price: '535/kg',
+        Img: './img/cheese.jpg'
+    },
+    {
+        id:9,
+        //Title:"Lassi",
+        Cat: 'Lassi',
+        Price: '90/l',
+        Img: './img/lassi.jpg'
+    },
+    /*{
+        id:10,
+        Title:"Yogurt",
+        //Cat: 'Gaming',
+        Price: '5036',
+        Img: './img/yogurt.jpg'
+    },
+    {
+        id:11,
+        Title:"Whipped Cream",
+        //Cat: 'Electronics',
+        Price: '198',
+        Img: './img/whippedcream.jpg'
+    },
+    {
+        id:12,
+        Title:"Whipped Cream",
+        //Cat: 'Electronics',
+        Price: '793',
+        Img: './img/whippedcream.jpg'
+    },*/
+]
+export default Productdetail;
